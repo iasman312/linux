38877e8d0b2f4822228fa2fe7d3f9383a4be8d30
iasman312 <iasman312@gmail.com>
2024-03-01 05:52:30 +0000

    First commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is a simple Bash script
+# It greets the user with their name
+
+# Prompt the user to enter their name
+echo "Hello! Please enter your name:"
+
+# Read the input from the user
+read name
+
+# Print a greeting message with the user's name
+echo "Hello, $name! Welcome to the world of Bash scripting!"
